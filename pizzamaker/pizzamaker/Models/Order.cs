96a8502f8bdc60db81efcf67947102abd9eb6cea
Misi <mpekar55@gmail.com>
2019-04-17 16:05:48 +0200

    Order singleton class készítése.

---
+++ pizzamaker/pizzamaker/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizzamaker.Models
+{
+    class Order:Food
+    {
+        private Order()
+        {
+            pizzaCondiments = new List<Food>();
+        }
+        static Order order = null;
+
+        public static Order getInstance() {
+            if (order == null) {
+                lock (order) {
+                    if (order == null) order = new Order();
+                }
+                
+            }
+            return order;
+        }
+        private List<Food> pizzaCondiments;
+
+    }
+}
