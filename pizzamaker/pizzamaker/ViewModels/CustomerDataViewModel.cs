e64d10cae0d3a827ee4e82cf50e1384798aa1c9a
Misi <mpekar55@gmail.com>
2019-04-18 12:01:33 +0200

    CustomerInformation View validaton beállítása és oldalak közötti váltó gombok előkészítése.

--- pizzamaker/pizzamaker/ViewModels/CustomerDataViewModel.cs
+++ pizzamaker/pizzamaker/ViewModels/CustomerDataViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using pizzamaker.Models.Exceptions;
+using System.Windows;
 
 namespace pizzamaker.ViewModels
 {
@@ -15,20 +17,89 @@ namespace pizzamaker.ViewModels
             this.mainWindow = mainWindow;
         }
         StartUpViewModel mainWindow;
-        public string NameError { get; set; } 
-        public string EmailError { get; set; } 
-        public string MobilError { get; set; } 
-        public string AddressError { get; set; }
+        private string _nameError;
+        public string NameError
+        {
+            get { return _nameError; }
+            set {
+                _nameError = value;
+                NotifyOfPropertyChange(() => NameError);
+            }
+        }
+        private string _emailError;
+        public string EmailError
+        {
+            get { return _emailError; }
+            set
+            {
+                _emailError = value;
+                NotifyOfPropertyChange(()=>EmailError);
+            }
+        }
+        private string _mobilError;
+        public string MobilError {
+            get {
+                return _mobilError;
+            }
+            set {
+                _mobilError = value;
+                NotifyOfPropertyChange(() => MobilError);
+            }
+        }
+        private string _addressError;
+        public string AddressError {
+            get{
+                return _addressError;
+            }
+            set {
+                _addressError = value;
+                NotifyOfPropertyChange(() => AddressError);
+            }
+        }
 
         public string NameTextBox { get; set; } 
         public string EmailTextBox { get; set; } 
         public string MobileTextBox { get; set; } 
         public string AddressTextBox { get; set; }
 
+        private void ResetErrorsTexts() {
+            NameError = "";
+            EmailError = "";
+            MobilError = "";
+            AddressError = "";
+        }
+
         public void LoadNextView() {
             Customer customer = new Customer();
+            customer.Name = NameTextBox;
+            customer.Email = EmailTextBox;
+            customer.MobileNumber = MobileTextBox;
+            customer.Address = AddressTextBox;
+            try {
+                ResetErrorsTexts();
+                if(customer.MassValidation()) mainWindow.LoadNextView(); 
+            }
+            catch (CustomerNameErrorException)
+            {
+                NameError = "Invalid name. eg.:John Smith";
+            }
+            catch (CustomerEmailErrorException)
+            {
+                EmailError = "Invalid email. eg.:example@example.com";
+            }
+            catch (CustomerMobileNumberErrorException)
+            {
+                MobilError = "Invalid mobile number. eg.:+36701234567";
+            }
+            catch (CustomerAddressErrorException)
+            {
+                AddressError = "Invalid address. eg.: 3390 Eger, Leányka utca 2.";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
 
-            mainWindow.LoadNextView();
         }
 
     }
