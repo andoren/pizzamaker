43c2a6c36b52f720f4535094a74c2da993902258
Misi <mpekar55@gmail.com>
2019-04-21 09:46:00 +0200

    Adatbázisban tészták létrehozása.

--- Database script/fullscript.sql
+++ Database script/fullscript.sql
@@ -1,5 +1,12 @@
 drop schema if exists pizzamaker;
 create schema pizzamaker;
+create table pizzamaker.dough(
+id int  primary key AUTO_INCREMENT,
+name varchar(100)  not null unique,
+description varchar(1000)  not null,
+price  double not null default 0,
+rawpicture mediumblob  not null
+);
 create table pizzamaker.sauce
 (
 id int  primary key AUTO_INCREMENT,
