41148252323bbed3a308dee91d9bb0ea9ce70e34
Misi <mpekar55@gmail.com>
2019-04-21 09:47:13 +0200

    Adatbázisban feltétek létrehozása.

--- Database script/fullscript.sql
+++ Database script/fullscript.sql
@@ -23,3 +23,11 @@ description varchar(1000)  not null,
 price  double not null default 0,
 rawpicture mediumblob  not null
 );
+create table pizzamaker.topping
+(
+id int  primary key AUTO_INCREMENT,
+name varchar(100)  not null unique,
+description varchar(1000)  not null,
+price  double not null default 0,
+rawpicture mediumblob  not null
+);
