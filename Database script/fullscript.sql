36e48783f8da2dc248f11a026d0512d622ed7a21
Misi <mpekar55@gmail.com>
2019-04-21 23:38:27 +0200

    pár kép cseréje az adatbázísból

--- Database script/fullscript.sql
+++ Database script/fullscript.sql
@@ -24,11 +24,12 @@ insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Glu
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Crusty dough","dough","This is a regular dough where we add all the necessary ingridients what is needed to a good pizza dough with crusty end.",1.25,load_file("C:/Pictures/Doughs/normal.jpg"));
 
 /*sauces*/
-insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Tomato sauce","sauce","This is a regular souce where we add all the necessary ingridients what is needed to a good pizza sauce",0.55,load_file("C:/Pictures/Sauces/tomato.png"));
-insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Mustard","sauce","It's stone-ground for a traditional tangy flavor. Ingredients: Distilled White Vinegar, Mustard Seed, Water, Salt, Turmeric, Natural Flavor and Spices. ",0.7,load_file("C:/Pictures/Sauces/mustard.png"));
-insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Spicy sauce","sauce","Add some spice to your life with Cholula Hot Sauce Original. This authentic hot sauce is imported straight from Mexico. It adds a fiery taste to your meals whenever you need an extra kick of spice. ",1.00,load_file("C:/Pictures/Sauces/spicy.png"));
-insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Sour cream","sauce","It could be the fresh and delicious taste, the rich and creamy texture, or that it’s made from simple, wholesome ingredients - with no artificial additives or preservatives.",0.45,load_file("C:/Pictures/Sauces/sour.png"));
-insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Nutella","sauce","SpreadMade with skim milk and cocoaOver 100 hazelnuts per jarContains no artificial colors or preservatives Nutella Hazelnut Spread",1.25,load_file("C:/Pictures/Sauces/nutella.png"));
+insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Tomato sauce","sauce","This is a regular souce where we add all the necessary ingridients what is needed to a good pizza sauce",0.55,load_file("C:/Pictures/Sauces/tomato.jpg"));
+insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Mustard","sauce","It's stone-ground for a traditional tangy flavor. Ingredients: Distilled White Vinegar, Mustard Seed, Water, Salt, Turmeric, Natural Flavor and Spices. ",0.7,load_file("C:/Pictures/Sauces/mustard.jpg"));
+insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Spicy sauce","sauce","Add some spice to your life with Cholula Hot Sauce Original. This authentic hot sauce is imported straight from Mexico. It adds a fiery taste to your meals whenever you need an extra kick of spice. ",1.00,load_file("C:/Pictures/Sauces/spicy.jpg"));
+insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Sour cream","sauce","It could be the fresh and delicious taste, the rich and creamy texture, or that it’s made from simple, wholesome ingredients - with no artificial additives or preservatives.",0.45,load_file("C:/Pictures/Sauces/sour.jpg"));
+insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Nutella","sauce","SpreadMade with skim milk and cocoaOver 100 hazelnuts per jarContains no artificial colors or preservatives Nutella Hazelnut Spread",1.25,load_file("C:/Pictures/Sauces/nutella.jpg"));
+
 /*meats*/
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Chicken breast stripes","meat","Fresh Class A free range corn fed skinless chicken breast fillets.",0.55,load_file("C:/Pictures/Meats/chicken.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Fish","meat","Freshly cought fish from the sea. We recommand you to try it.",0.7,load_file("C:/Pictures/Meats/fish.jpg"));
@@ -36,17 +37,19 @@ insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Por
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Pulled pork","meat","Pulled Pork is an American culinary dish that originated in the southern US states which uses shredded barbecued pork shoulder as the main ingredient.",1.25,load_file("C:/Pictures/Meats/pulled.png"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Salami slices","meat","Sliced seasoned pork salami. This perfectly seasoned Italian salami is traditionally dry cured by a Italian family run company with 8 generations of expertise.",0.45,load_file("C:/Pictures/Meats/salami.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Sausage","meat","Pork sausage meat filled into natural pork casings.",1.25,load_file("C:/Pictures/Meats/sausage.jpg"));
+
 /*Topping*/
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Tomato","topping","Freshly sliced tomato is always a perfect choice",0.1,load_file("C:/Pictures/Toppings/tomato.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Mushroom ","topping","Freshly sliced mushroom is always a perfect choice",0.2,load_file("C:/Pictures/Toppings/mushroom.jpg"));
-insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Pepper","topping","Freshly sliced pepper is always a perfect choice",0.3,load_file("C:/Pictures/Toppings/pepper.png"));
+insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Pepper","topping","Freshly sliced pepper is always a perfect choice",0.3,load_file("C:/Pictures/Toppings/pepper.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Pineapple","topping","The most valami ingredient in the world",1.1,load_file("C:/Pictures/Toppings/pineapple.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Banana","topping","This ingridients is for the nutella souce",1.2,load_file("C:/Pictures/Toppings/banana.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Olive","topping","Fresh olive straight from the shop.",0.4,load_file("C:/Pictures/Toppings/olive.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Rocket","topping","Fresh rocket",0.1,load_file("C:/Pictures/Toppings/rocket.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Corn","topping","Caned corn",0.2,load_file("C:/Pictures/Toppings/corn.jpg"));
-insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Bean","topping","Caned bean",0.3,load_file("C:/Pictures/Toppings/bean.png"));
+insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Bean","topping","Caned bean",0.3,load_file("C:/Pictures/Toppings/bean.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Eggs","topping","Boiled egg slices",0.4,load_file("C:/Pictures/Toppings/egg.jpg"));
+
 /*cheese*/                        
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Mozzarella","cheese","Shreded mozzarela",0.5,load_file("C:/Pictures/Cheese/moz.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Mix","cheese","All kind of cheese what you can find in our cheese menu",0.6,load_file("C:/Pictures/Cheese/mix.jpg"));
@@ -54,4 +57,5 @@ insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Eda
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Parmegano","cheese","Finest parmegano what you can find in the world. You should really try this.",1.9,load_file("C:/Pictures/Cheese/parm.jpg"));
 insert into pizzamaker.foods(name,type,description,price,rawpicture) values("Trapist","cheese","Regular trapist cheese",0.4,load_file("C:/Pictures/Cheese/trap.jpg"));
 
-select id,name,description,price from pizzamaker.foods;
+insert into pizzamaker.logs(class,what,result)values("proba","insert","ez egy próba logolás");
+select * from pizzamaker.logs;
